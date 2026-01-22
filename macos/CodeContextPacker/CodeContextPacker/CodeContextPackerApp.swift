f6c47d92cb48b16c86a78936ec42dcdc5e17de9d
drfranken <daranic9@gmail.com>
2026-01-22 16:47:27 +0900

    chore: scaffold macOS SwiftUI app

---
+++ macos/CodeContextPacker/CodeContextPacker/CodeContextPackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeContextPackerApp.swift
+//  CodeContextPacker
+//
+//  Created by drfranken on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeContextPackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
